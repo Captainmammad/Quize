2ea2df0516712bd358a22ee73a7a770da9379eb3
Captainmammad <dyayymhmdhsyn726@gmail.com>
2025-10-01 06:23:13 +0330

    fix: gradle build scripts for Bitrise

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,12 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    // Flutter Gradle Plugin باید بعد از Android و Kotlin بیاد
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 import java.util.Properties
 import java.io.FileInputStream
 
-// بارگذاری key.properties برای امضای Release
 val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("key.properties")
 if (keystorePropertiesFile.exists()) {
@@ -18,7 +16,6 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.mho.quize"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "29.0.14033849"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -26,7 +23,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "11"
     }
 
     defaultConfig {
@@ -62,36 +59,19 @@ android {
     }
 }
 
-// اضافه کردن Maven محلی برای JARهای مورد نیاز
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven {
-            url = uri("C:/android-maven-repo") // مسیر JARهای lint-checks و uast و intellij-core
-        }
-    }
-}
-
 dependencies {
-    // Native JARها
-    implementation(files("libs/armeabi_v7a_release-1.0.0-ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2b7.jar"))
-    implementation(files("libs/arm64_v8a_release-1.0.0-ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2b7.jar"))
-    implementation(files("libs/x86_64_release-1.0.0-ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2b7.jar"))
+    implementation(files("libs/armeabi_v7a_release.jar"))
+    implementation(files("libs/arm64_v8a_release.jar"))
+    implementation(files("libs/x86_64_release.jar"))
 
-    // Dependencies با exclude برای Kotlin DSL
     implementation("com.android.tools.lint:lint-checks:31.9.1")
-
     implementation("com.android.tools.external.org-jetbrains:uast:31.9.1") {
         exclude(group = "org.jetbrains", module = "annotations")
     }
-
     implementation("com.android.tools.external.com-intellij:intellij-core:31.9.1") {
         exclude(group = "org.jetbrains", module = "annotations")
     }
 }
-
-
 
 flutter {
     source = "../.."
