1f43009959cbfe8e146c9e96a43bdf2a440b3a0b
Captainmammad <143606965+Captainmammad@users.noreply.github.com>
2025-10-01 18:35:53 +0330

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,23 +1,6 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-import java.io.File
-
 android {
     namespace = "com.mho.quize"
     compileSdk = flutter.compileSdkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 
     defaultConfig {
         applicationId = "com.mho.quize"
@@ -29,14 +12,11 @@ android {
 
     signingConfigs {
         create("release") {
-            // مسیر فایل keystore که در Script Step دانلود شد
-            storeFile = File("/bitrise/keystore/my-release-key.jks")
-            storePassword = System.getenv("BITRISEIO_ANDROID_KEYSTORE_PASSWORD")
-                ?: error("Keystore password not set!")
-            keyAlias = System.getenv("BITRISEIO_ANDROID_KEYSTORE_ALIAS")
-                ?: error("Key alias not set!")
-            keyPassword = System.getenv("BITRISEIO_ANDROID_KEYSTORE_PRIVATE_KEY_PASSWORD")
-                ?: error("Key password not set!")
+            // مسیر keystore نسبت به app/build.gradle.kts
+            storeFile = file("../app/my-release-key.jks")
+            storePassword = "@Mho1389"
+            keyAlias = "my-key-alias"
+            keyPassword = "@Mho1389"
         }
     }
 
@@ -51,11 +31,16 @@ android {
             )
         }
         getByName("debug") {
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("release") // اگه میخوای دیباگ هم با همون keystore باشه
         }
     }
-}
 
-flutter {
-    source = "../.."
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
